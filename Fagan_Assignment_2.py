dfb454c4054acbf2f0ff933b1fc0b344ee3f6f5f
Emily Fagan <emilygfagan@gmail.com>
2024-02-11 14:53:12 -0600

    Update Fagan_Assignment_2.py

--- Fagan_Assignment_2.py
+++ Fagan_Assignment_2.py
@@ -1,8 +1,8 @@
 # Assignment 2: Write a function area_of_circle which returns the area of a circle of radius r.
 
-r_str = input("Enter radius: ")
+r_str = input("Enter radius: ") # accepting user input and naming it r_str
 
-r = int(r_str)
+r = int(r_str) # type casting user input from string to integer and naming it r
 
 def area_circle(r):
     pi = 3.14159
