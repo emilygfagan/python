e5b2ce16b37421caba8027fe5baa572f6f683bcb
Emily Fagan <emilygfagan@gmail.com>
2024-02-11 15:49:19 -0600

    Update Fagan_Assignment_4.py

--- Fagan_Assignment_4.py
+++ Fagan_Assignment_4.py
@@ -1,4 +1,14 @@
 # Write a function which is given an exam mark, and it returns a string according to the grade scheme.
+""" 
+Grade Scheme: 
+    First --> m >= 75
+    Upper Second --> [70-75)
+    Second --> [60-70)
+    Third --> [50-60)
+    F1 Supp --> [45-50)
+    F2 --> [40-45)
+    F3 --> m < 40
+"""
 
 xs = [83, 75, 74.9, 70, 69.9, 65, 60, 59.9, 55, 50, 49.9, 45, 44.9, 40, 39.9, 2, 0]
 
